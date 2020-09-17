7250e15123ec5a8ad7594fb7c5e66d29c79205df
kevin ramadhan <you@example.com>
2020-09-17 17:03:44 +0700

    myproject

---
+++ script.js
@@ -0,0 +1,13 @@
+const sideNav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+  //   indicators: false, kalau mau pakai lingkaran hidupkan script ini
+  height: 500,
+  transition: 600,
+  interval: 4000,
+});
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
